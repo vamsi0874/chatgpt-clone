81e4c350f440ee3e4e1641e5ec4e8a1248f5133e
vamsi0874 <vamsim0874>
2024-10-01 19:50:43 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -174,9 +174,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 console.log(__filename)
 
+
 app.use(
   cors({
-     origin: true,
+     origin: 'https://chatgpt-clone-client.onrender.com',
     credentials: true,
   })
 );
