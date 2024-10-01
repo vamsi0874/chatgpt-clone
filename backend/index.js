42bb975f9a8419a8f46b6a261ac643f263f42b3d
vamsi0874 <vamsim0874>
2024-10-01 18:40:47 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -333,10 +333,10 @@ app.use((err, req, res, next) => {
 });
 
 // PRODUCTION
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
 app.listen(port, () => {
